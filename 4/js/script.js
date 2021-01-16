0c9d6618d07f0e8f8de8e30d3070d6d86a6298fe
bazunaka-macbook <bazunaka32@gmail.com>
2021-01-16 10:29:21 +0300

    add jshint config. modify script file.

--- 4/js/script.js
+++ 4/js/script.js
@@ -1,1 +1,18 @@
-console.log(1);
+let number = 5; //изменяемая переменная 
+const leftBorderWidth = 1; //константа
+
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+if (hourNow > 18) {
+    greeting = 'Добрый вечер!';
+} else if (hourNow > 12) {
+    greeting = 'Добрый день!';
+} else if (hourNow > 0) {
+    greeting = 'Доброе утро!';
+} else {
+    greeting = 'Приветствуем!';
+}
+
+console.log(greeting);
